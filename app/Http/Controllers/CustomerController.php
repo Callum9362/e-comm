dc75b0fc1f28534ed6392c7b2fbe7bfaf8411a7e
Callum Gill <callum.gill.1993.cg@gmail.com>
2023-01-31 08:59:17 -0800

    Added improved error handling

--- app/Http/Controllers/CustomerController.php
+++ app/Http/Controllers/CustomerController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Customer;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class CustomerController extends Controller
 {
@@ -14,13 +15,17 @@ class CustomerController extends Controller
 
    public function store(Request $request)
    {
-       $request->validate([
+       $validator = Validator::make($request->all(), [
            'first_name' => 'required|max:255',
            'last_name' => 'required|max:255',
            'email' => 'required|email|unique:customers',
            'birth' => 'required',
            'telephone' => 'required'
        ]);
+
+       if ($validator->fails()) {
+           return response()->json(['errors' => $validator->errors()], 422);
+       }
 
        $customer = new Customer;
        $customer->first_name = $request->get('first_name');
