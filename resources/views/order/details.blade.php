130458fbf024c756cf2ab93516bc43ea4c549a2d
Callum Gill <callum.gill.1993.cg@gmail.com>
2023-03-06 08:08:49 -0800

    Made an effort to clean up Tailwind CSS classes

--- resources/views/order/details.blade.php
+++ resources/views/order/details.blade.php
@@ -14,24 +14,24 @@
                         <h2 class="text-lg font-medium mb-4 text-center">Customer Information</h2>
                         <div class="flex flex-col gap-3">
                             <label class="text-gray-500">Full Name</label>
-                            <input type="text" value="{{ $order->customer->first_name . " " . $order->customer->last_name }}" class="border bg-gray-100 border-gray-300 rounded-lg py-2 px-3 focus:outline-none focus:ring-2 focus:ring-blue-500" readonly>
+                            <input type="text" value="{{ $order->customer->first_name . " " . $order->customer->last_name }}" class="info-input" readonly>
                             <label class="text-gray-500">Date of Birth</label>
-                            <input type="text" value="{{ $order->customer->birth }}" class="border bg-gray-100 border-gray-300 rounded-lg py-2 px-3 focus:outline-none focus:ring-2 focus:ring-blue-500" readonly>
+                            <input type="text" value="{{ $order->customer->birth }}" class="info-input" readonly>
                             <label class="text-gray-500">Email</label>
-                            <input type="text" value="{{ $order->customer->email }}" class="border bg-gray-100 border-gray-300 rounded-lg py-2 px-3 focus:outline-none focus:ring-2 focus:ring-blue-500" readonly>
+                            <input type="text" value="{{ $order->customer->email }}" class="info-input" readonly>
                             <label class="text-gray-500">Telephone</label>
-                            <input type="text" value="{{ $order->customer->telephone }}" class="border bg-gray-100 border-gray-300 rounded-lg py-2 px-3 focus:outline-none focus:ring-2 focus:ring-blue-500" readonly>
+                            <input type="text" value="{{ $order->customer->telephone }}" class="info-input" readonly>
                         </div>
                     </div>
                     <div class="w-1/2 bg-white rounded-lg shadow-lg px-6 py-4">
                         <h2 class="text-lg font-medium mb-4 text-center">Payment Information</h2>
                         <div class="flex flex-col gap-3">
                             <label class="text-gray-500">Method</label>
-                            <input type="text" value="{{ $order->payment->method }}" class="border bg-gray-100 border-gray-300 rounded-lg py-2 px-3 focus:outline-none focus:ring-2 focus:ring-blue-500" readonly>
+                            <input type="text" value="{{ $order->payment->method }}" class="info-input" readonly>
                             <label class="text-gray-500">Status</label>
-                            <input type="text" value="{{ $order->payment->status }}" class="border @if($order->payment->status === 'Complete') bg-green-200 @else bg-red-500 @endif border-gray-300 rounded-lg py-2 px-3 focus:outline-none focus:ring-2 focus:ring-blue-500" readonly>
+                            <input type="text" value="{{ $order->payment->status }}" class="info-input @if($order->payment->status === 'Complete') bg-green-200 @else bg-red-500 @endif" readonly>
                             <label class="text-gray-500">Date</label>
-                            <input type="text" value="{{ $order->payment->created_at->format('M d, Y \a\t g:i A') }}" class="border bg-gray-100 border-gray-300 rounded-lg py-2 px-3 focus:outline-none focus:ring-2 focus:ring-blue-500" readonly>
+                            <input type="text" value="{{ $order->payment->created_at->format('M d, Y \a\t g:i A') }}" class="info-input" readonly>
                         </div>
                     </div>
                 </div>
@@ -41,11 +41,11 @@
                         <h2 class="text-lg font-medium mb-4 text-center">Shipping</h2>
                         <div class="flex flex-col gap-3">
                             <label class="text-gray-500">Vendor</label>
-                            <input type="text" value="{{ $order->shipping->method }}" class="border bg-gray-100 border-gray-300 rounded-lg py-2 px-3 focus:outline-none focus:ring-2 focus:ring-blue-500" readonly>
+                            <input type="text" value="{{ $order->shipping->method }}" class="info-input" readonly>
                             <label class="text-gray-500">Status</label>
-                            <input type="text" value="{{ $order->shipping->status }}" class="border bg-gray-100 border-gray-300 rounded-lg py-2 px-3 focus:outline-none focus:ring-2 focus:ring-blue-500" readonly>
+                            <input type="text" value="{{ $order->shipping->status }}" class="info-input" readonly>
                             <label class="text-gray-500">Sent Out</label>
-                            <input type="text" value="{{ $order->shipping->date }}" class="border bg-gray-100 border-gray-300 rounded-lg py-2 px-3 focus:outline-none focus:ring-2 focus:ring-blue-500" readonly>
+                            <input type="text" value="{{ $order->shipping->date }}" class="info-input" readonly>
                         </div>
                     </div>
 
@@ -53,15 +53,15 @@
                         <h2 class="text-lg font-medium mb-4 text-center">Address Information</h2>
                         <div class="flex flex-col gap-3">
                             <label class="text-gray-500">Street</label>
-                            <input type="text" value="{{ $order->shipping->address->street }}" class="border bg-gray-100 border-gray-300 rounded-lg py-2 px-3 focus:outline-none focus:ring-2 focus:ring-blue-500" readonly>
+                            <input type="text" value="{{ $order->shipping->address->street }}" class="info-input" readonly>
                             <label class="text-gray-500">City</label>
-                            <input type="text" value="{{ $order->shipping->address->city }}" class="border bg-gray-100 border-gray-300 rounded-lg py-2 px-3 focus:outline-none focus:ring-2 focus:ring-blue-500" readonly>
+                            <input type="text" value="{{ $order->shipping->address->city }}" class="info-input" readonly>
                             <label class="text-gray-500">State</label>
-                            <input type="text" value="{{ $order->shipping->address->state }}" class="border bg-gray-100 border-gray-300 rounded-lg py-2 px-3 focus:outline-none focus:ring-2 focus:ring-blue-500" readonly>
+                            <input type="text" value="{{ $order->shipping->address->state }}" class="info-input" readonly>
                             <label class="text-gray-500">ZIP</label>
-                            <input type="text" value="{{ $order->shipping->address->zip }}" class="border bg-gray-100 border-gray-300 rounded-lg py-2 px-3 focus:outline-none focus:ring-2 focus:ring-blue-500" readonly>
+                            <input type="text" value="{{ $order->shipping->address->zip }}" class="info-input" readonly>
                             <label class="text-gray-500">Country</label>
-                            <input type="text" value="{{ $order->shipping->address->country }}" class="border bg-gray-100 border-gray-300 rounded-lg py-2 px-3 focus:outline-none focus:ring-2 focus:ring-blue-500" readonly>
+                            <input type="text" value="{{ $order->shipping->address->country }}" class="info-input" readonly>
                         </div>
                     </div>
                 </div>
