984ee245eb0401bd54ff1cf78d41f348279f8a27
Callum Gill <callum.gill.1993.cg@gmail.com>
2023-03-11 11:46:32 -0800

    Finished up the livewire component to create a coupon

--- app/Http/Livewire/CreateCouponForm.php
+++ app/Http/Livewire/CreateCouponForm.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Livewire;
 
+use App\Models\Coupon;
 use Livewire\Component;
 
 class CreateCouponForm extends Component
@@ -31,5 +32,28 @@ class CreateCouponForm extends Component
     public function createCoupon()
     {
         $this->validate();
+
+        $coupon = new Coupon;
+        $coupon->code = $this->code;
+        $coupon->discount = $this->discount;
+        $coupon->type = $this->type;
+        $coupon->start_date = $this->from;
+        $coupon->end_date = $this->to;
+        $coupon->usage_limit = $this->limit;
+
+        $coupon->save();
+
+        $this->success = 'Coupon was created successfully!';
+        $this->refresh();
+    }
+
+    public function refresh()
+    {
+        $this->code = "";
+        $this->discount = "";
+        $this->type = "";
+        $this->from = "";
+        $this->to = "";
+        $this->limit = "";
     }
 }
--- resources/views/livewire/create-coupon-form.blade.php
+++ resources/views/livewire/create-coupon-form.blade.php
@@ -1,4 +1,14 @@
 <div class="grid place-items-center mt-2">
+    @if ($success)
+        <div class="success-alert" role="alert">
+            <div>
+                {{ $success }}
+                <button class="ml-2 text-white" wire:click="$set('success', null)">
+                    <i class="fas fa-times"></i>
+                </button>
+            </div>
+        </div>
+    @endif
     <div class="w-1/2 bg-white rounded-lg shadow-lg px-6 py-4 mr-2">
         <h2 class="text-lg font-medium mb-4 text-center">Create Coupon</h2>
         <div class="flex flex-col gap-3">
@@ -28,14 +38,14 @@
                     <label class="text-gray-500">Valid From</label>
                 </div>
                 <div>
-                    <input type="text" name="from" wire:model="from" value="{{ old('from') }}" class="@error('from') border-red-500 @enderror w-full form-input">
+                    <input  type="datetime-local" name="from" wire:model="from" value="{{ old('from') }}" class="@error('from') border-red-500 @enderror w-full form-input">
                     @error('from') <span class="text-red-500">{{ $message }}</span> @enderror
                 </div>
                 <div>
                     <label class="text-gray-500">Valid Until</label>
                 </div>
                 <div>
-                    <input type="text" name="to" wire:model="to" value="{{ old('to') }}" class="@error('to') border-red-500 @enderror w-full form-input">
+                    <input type="datetime-local" name="to" wire:model="to" value="{{ old('to') }}" class="@error('to') border-red-500 @enderror w-full form-input">
                     @error('to') <span class="text-red-500">{{ $message }}</span> @enderror
                 </div>
                 <div>
