9fd1e24ed9830221edb0b9942b2386670c2eccfe
Callum Gill <callum.gill.1993.cg@gmail.com>
2023-03-17 16:27:33 -0700

    Improve the search bar

--- app/Http/Livewire/SearchBar.php
+++ app/Http/Livewire/SearchBar.php
@@ -10,17 +10,17 @@ class SearchBar extends Component
     public $query;
     public $searchResults;
 
+    public function render()
+    {
+        return view('livewire.search-bar')
+            ->with('searchResults', $this->searchResults);
+    }
+
     public function updatedQuery()
     {
         $this->searchResults = Order::where('order_number', 'like', '%'.$this->query.'%')
             ->take(10)
             ->get()
             ->toArray();
-    }
-
-    public function render()
-    {
-        return view('livewire.search-bar')
-            ->with('searchResults', $this->searchResults);
     }
 }
--- resources/views/livewire/search-bar.blade.php
+++ resources/views/livewire/search-bar.blade.php
@@ -1,6 +1,6 @@
 <div class="relative">
     <input
-        type="text"
+        type="search"
         class="w-full px-4 m-2 py-2 rounded-lg shadow-sm focus:outline-none focus:shadow-outline text-gray-600 font-medium"
         placeholder="Search orders..."
         wire:model.debounce.500ms="query">
@@ -8,7 +8,7 @@
         <div class="absolute top-10 left-0 w-full bg-white rounded-lg shadow-lg z-50">
             @foreach ($searchResults as $result)
                 <a href="{{ route('order-details', ['id' => $result['id']]) }}"
-                   class="block px-4 py-2 hover:bg-gray-100 transition ease-in-out duration-150">{{ $result['order_number'] }}</a>
+                   class="block px-4 py-2 hover:bg-gray-100 transition ease-in-out duration-150">#{{ $result['order_number'] }}</a>
             @endforeach
         </div>
     @endif
