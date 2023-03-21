67e39e182fb523e0e57cb9b8a8ce2288c3122738
Callum Gill <callum.gill.1993.cg@gmail.com>
2023-03-20 18:02:56 -0700

    Refactored some livewire classes for easier navigation

--- app/Http/Livewire/coupon/CreateCouponForm.php
+++ app/Http/Livewire/coupon/CreateCouponForm.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Http\Livewire;
+namespace App\Http\Livewire\coupon;
 
 use App\Models\Coupon;
 use Livewire\Component;
--- app/Http/Livewire/order/CancelledOrderTable.php
+++ app/Http/Livewire/order/CancelledOrderTable.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Http\Livewire;
+namespace App\Http\Livewire\order;
 
 use App\Models\Order;
 use Livewire\Component;
